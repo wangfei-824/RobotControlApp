fef4a001a6b6a46cd08b7bf371bf92cd4456a33f
wangfei-824 <651277897@qq.com>
2019-08-30 18:56:24 +0800

    Initialize the project.

---
+++ RobotControlApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RobotControlApp
+//
+//  Created by Lucky on 19/8/30.
+//  Copyright © 2019年 Felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
